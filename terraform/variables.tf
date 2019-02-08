7f90f4c00b5a528ecd493185745184475b3d4645
jeff oconnell <jeffo@rulez.com>
2019-02-08 00:21:08 -0500

    terraform variable and output descriptions, types

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,24 +1,34 @@
 
 variable "function_name" {
+  description = "name for the lambda function"
   default = "ecstatic"
+  type = "string"
 }
 
 variable "description" {
+  description = "description for the lambda function"
   default = "monitor and update ecs agents"
+  type = "string"
+}
+
+variable "s3_bucket" {
+  description = "S3 bucket with the initial lambda zip resides"
+  default = "code.firstlook.media"
+  type = "string"
+}
+
+variable "s3_key" {
+  description = "S3 key for the initial lambda zip"
+  default = "ecstatic/ecstatic.zip"
+  type = "string"
 }
 
 variable "subnet_ids" {
+  description = "list of subnet ids for the function"
   type = "list"
 }
 
 variable "security_group_ids" {
+  description = "list of security group ids for the function"
   type = "list"
-}
-
-variable "s3_bucket" {
-  default = "code.firstlook.media"
-}
-
-variable "s3_key" {
-  default = "ecstatic/ecstatic.zip"
 }
